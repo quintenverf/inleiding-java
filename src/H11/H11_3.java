52a8c43b38a890086df03984032fb5eb66c72493
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-05 13:42:16 +0200

    H11.3 KLAAR

--- src/H11/H11_3.java
+++ src/H11/H11_3.java
@@ -9,6 +9,26 @@ public class H11_3 extends Applet {
 
     }
     public void paint(Graphics g) {
+        g.drawString("1, ",10,20);
+        g.drawString("1, ",10,40);
+        g.drawString("2, ",10,60);
+        g.drawString("3, ",10,80);
+        g.drawString("5, ",10,100);
+        g.drawString("8, ",10,120);
+        g.drawString("13, ",10,140);
+        g.drawString("21, ",10,160);
+        g.drawString("34, ",10,180);
+        g.drawString("55, ",10,200);
+        g.drawString("89, ",10,220);
+        g.drawString("144, ",10,240);
+        g.drawString("233, ",10,260);
+        g.drawString("377, ",10,280);
+        g.drawString("610, ",10,300);
+        g.drawString("987, ",10,320);
+        g.drawString("1597, ",10,340);
+
+
+
 
     }
 }
