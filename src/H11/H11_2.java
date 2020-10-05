9934d68ce72ef8f3217fabe58671525d72a16fdf
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-05 13:34:41 +0200

    H11.2 KLAAR

--- src/H11/H11_2.java
+++ src/H11/H11_2.java
@@ -1,4 +1,26 @@
 package H11;
 
-public class H11_2 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11_2 extends Applet {
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        setBackground(Color.black);
+        g.setColor(Color.white);
+        g.drawString("20, ",10,20);
+        g.drawString(" 19, ", 30,20);
+        g.drawString(" 18, ", 50,20);
+        g.drawString(" 17, ", 70,20);
+        g.drawString(" 16, ", 90,20);
+        g.drawString(" 15, ", 110,20);
+        g.drawString(" 14, ", 130,20);
+        g.drawString(" 13, ", 150,20);
+        g.drawString(" 12, ", 170,20);
+        g.drawString(" 11, ", 190,20);
+        g.drawString(" 10, ", 210,20);
+    }
 }
---
+++ src/H11/H11_3.java
@@ -0,0 +1,14 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11_3 extends Applet {
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+
+    }
+}
