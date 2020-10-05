091463e4da600e0bbb32cfa2b74c783fe7112131
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-05 13:26:45 +0200

    H11.1 KLAAR

--- src/H11/H11_1.java
+++ src/H11/H11_1.java
@@ -10,6 +10,17 @@ public class H11_1 extends Applet {
     }
 
     public void paint(Graphics g) {
-
+        setBackground(Color.black);
+        g.setColor(Color.white);
+        g.drawLine(10,200,10,10);
+        g.drawLine(20,200,20,10);
+        g.drawLine(30,200,30,10);
+        g.drawLine(40,200,40,10);
+        g.drawLine(50,200,50,10);
+        g.drawLine(60,200,60,10);
+        g.drawLine(70,200,70,10);
+        g.drawLine(80,200,80,10);
+        g.drawLine(90,200,90,10);
+        g.drawLine(100,200,100,10);
     }
 }
