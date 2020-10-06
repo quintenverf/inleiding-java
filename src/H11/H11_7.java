b35986793607133ffda3b56b3171ee17a702d2d1
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-06 08:10:28 +0200

    h11.7 committed

--- src/H11/H11_7.java
+++ src/H11/H11_7.java
@@ -3,7 +3,7 @@ package H11;
 import java.applet.Applet;
 import java.awt.*;
 
-public class H11_6 extends Applet {
+public class H11_7 extends Applet {
     int teller = 0;
     int y = 200;
     int x = 200;
@@ -11,16 +11,16 @@ public class H11_6 extends Applet {
     int height = 10;
 
     public void init() {
-    }
 
+    }
     public void paint(Graphics g) {
-        while (teller<5){
+        while (teller<50){
             g.drawOval(x, y, width, height);
             y -= 10;
             x -= 10;
-           width += 20;
-           height += 20;
-           teller ++;
+            width += 20;
+            height += 20;
+            teller++;
         }
     }
 }
