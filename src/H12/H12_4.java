56d5701037577a57a8e6593a57aa363548d6d43c
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-09 10:48:08 +0200

    12.4 committed**

--- src/H12/H12_4.java
+++ src/H12/H12_4.java
@@ -2,41 +2,50 @@ package H12;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class H12_4 extends Applet {
+    double inputdouble;
+    boolean typed = true;
     boolean found;
-    TextField tekstvak;
-    Button knop;
-    double[] numbers;
-    double search;
+    int index = 0;
+    String string = "";
+    TextField tekstvak = new TextField("", 10);
+    Button button = new Button("OK");
+    double[] numbers = { 3.1, 4.1, 59, 26, 53, 5.9};
 
     public void init() {
-        numbers = new double[10];
+        add(tekstvak);
+        add(button);
+        button.addActionListener(new knopListener() );
+    }
+    public void paint(Graphics g) {
+        if (typed == true){return;}
         found = false;
-        for (int teller = 0; teller < numbers.length ; teller++) {
-            numbers[teller] = 1 * teller + 1;
-
-            if (numbers[teller] == search){
+        int teller = 0;
+        while (teller < numbers.length){
+            if (numbers[teller] == inputdouble) {
                 found = true;
+                index = teller;
             }
             teller++;
         }
-        tekstvak = new TextField("", 10);
-        add(tekstvak);
-
-        knop = new Button("OK");
-        add(knop);
+        if (found == true){
+            g.drawString("uw waarde is gevonden. "+ index, 20,50);
+        }
+        else {
+            g.drawString("uw waarde is niet gevonden. " + index, 40,50);
+        }
     }
-    public void paint(Graphics g) {
-        for (int teller = 0; teller < numbers.length; teller++) {
-            g.drawString("" + numbers[teller], 50,20 * teller + 20);
 
-            if (found = true){
-                g.drawString("gevonden. ", 20,50);
-            }
-            else{
-                g.drawString("niet gevonden ", 20,50);
+        class knopListener implements ActionListener{
+            public void actionPerformed(ActionEvent e) {
+                typed = false;
+                string = tekstvak.getText();
+                inputdouble = Double.parseDouble(string);
+                repaint();
             }
         }
     }
-}
+
