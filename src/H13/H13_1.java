73c62e8090f0bb4e655061a5fa2272538e7594c8
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-21 13:10:33 +0200

    13_1 committed.

--- src/H13/H13_1.java
+++ src/H13/H13_1.java
@@ -6,12 +6,16 @@ import java.awt.*;
 public class H13_1 extends Applet {
 
     public void init() {
-        tekenDriehoek( 10,10, 20, 20, 30, 30);
+
     }
     public void paint(Graphics g) {
-    g.drawString(" ", 50, 50);
+
+        g.drawString("getekend en wel " , 50, 50);
+        tekenDriehoek(g,10,10, 20, 20, 30, 30);
+
     }
-    void tekenDriehoek( int x1, int y1, int x2, int y2, int x3, int y3) {
-        
+    void tekenDriehoek(Graphics quinten,int x1, int y1, int x2, int y2, int x3, int y3) {
+        quinten.drawPolygon(new int[]{100,200,300},new int[]{200,20,200},3);
+
     }
 }
