89ae2edcc5d986889100a94f454a109cdd5aad07
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-21 09:39:21 +0200

    13_1 committed  error manier om de code te zien, vinden.

--- src/H13/H13_1.java
+++ src/H13/H13_1.java
@@ -4,20 +4,12 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class H13_1 extends Applet {
-    double gem;
-
 
     public void init() {
-        int x1 = 10;
-        int y1 = 20;
-        int x2 = 10;
-        int y2 = 20;
-        int x3 = 10;
-        int y3 = 20;
-        tekenDriehoek( x1,y1, x2, y2, x3, y3);
+        tekenDriehoek( 10,10, 20, 20, 30, 30);
     }
     public void paint(Graphics g) {
-    g.drawString("getekend en wel " + gem, 50, 50);
+    g.drawString(" ", 50, 50);
     }
     void tekenDriehoek( int x1, int y1, int x2, int y2, int x3, int y3) {
         
---
+++ src/H13/H13_2.java
@@ -0,0 +1,14 @@
+package H13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H13_2 extends Applet {
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+
+    }
+}
