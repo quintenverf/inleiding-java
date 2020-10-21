9d61ce9434e7f46a723b35341252687db1e44821
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-21 18:30:24 +0200

    13_3 committed. only need to connect the walls

--- src/H13/H13_3.java
+++ src/H13/H13_3.java
@@ -2,13 +2,34 @@ package H13;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class H13_3 extends Applet {
+    Button knopRood = new Button("RED");
+    Button knopZwart = new Button("BLACK");
+
 
     public void init() {
+        add(knopRood);
+        knopRood.setBackground(Color.red);
+        add(knopZwart);
+        knopZwart.setBackground(Color.black);
 
+        knopRood.addActionListener(new knopRoodListener());
+        knopZwart.addActionListener(new knopZwartListener());
     }
     public void paint(Graphics g) {
 
+    }
+    class knopRoodListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    class knopZwartListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
