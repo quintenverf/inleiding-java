7ba7a5d7c40564c18524c5b61f9dc729f24f638f
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-21 17:44:26 +0200

    13_2 committed. breedte moet verschoven worden met 1

--- src/H13/H13_2.java
+++ src/H13/H13_2.java
@@ -5,10 +5,51 @@ import java.awt.*;
 
 public class H13_2 extends Applet {
 
+    int x = 0;
+    int y = 0;
+    int width = 40;
+    int height = 20;
+
     public void init() {
+        setBackground(Color.white);
+        setSize(1000, 500);
 
     }
-    public void paint(Graphics g) {
 
+    public void paint(Graphics g) {
+        tekenmuur(g, 10, 10);
+
+    }
+
+    void tekenmuur(Graphics quinten, int x1, int y1) {
+        quinten.setColor(Color.red);
+        for (int tellerHorizontal = 25; tellerHorizontal > 0; tellerHorizontal--) {
+            for (int tellervertical = 0; tellervertical < 25; tellervertical++) {
+                quinten.fillRect(x, y, width, height);
+                x += width;
+            }
+            y += 21;
+            x = 1;
+        }
+
+//        while (teller < 25) {
+//            while (teller2 < 25){
+//                quinten.drawRect(x, y, width, 20);
+//                teller2++;
+//                x += width;
+//            }
+//            y += 20;
+//            teller++;
+//            teller2 = 0;
+//            x = 0;
+//        }
+//        teller =0;
+//        while (teller < 25) {
+//            quinten.drawRect(x, y, width, 20);
+//            x += 40;
+//            y += 0;
+//            teller++;
+//        }
+//        x += width;
     }
 }
