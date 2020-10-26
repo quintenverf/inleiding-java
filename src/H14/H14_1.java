e2567183fa58a35a41968e0aa491b24cf3aff5d5
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-26 13:19:09 +0100

    14_1 it works

--- src/H14/H14_1.java
+++ src/H14/H14_1.java
@@ -8,41 +8,20 @@ import java.awt.event.ActionListener;
 public class H14_1 extends Applet {
     double r;
     int shuffle;
-    String s, tekst;
+    String  tekst;
     Button knop = new Button();
     boolean clicked;
 
 
     public void init() {
         clicked = false;
-//        if(clicked == true){
-//        r = Math.random();
-//        shuffle = (int)(r * 14 +2);
-//        switch(shuffle) {
-//            case 11:
-//                tekst = "jack";
-//                break;
-//            case 12:
-//                tekst = "queen";
-//                break;
-//            case 13:
-//                tekst = "king";
-//                break;
-//            case 14:
-//                tekst = "ace";
-//                break;
-//            default:
-//                tekst = "" + shuffle;
-//
-//                add(knop);
-//                knop.addActionListener(new KnopListener());
-//            }
-//        }
-//        repaint();
+        add(knop);
+        knop.addActionListener(new KnopListener());
+
     }
 
     public void paint(Graphics g) {
-        g.drawString(tekst, 50, 50);
+       g.drawString( "" + tekst, 50, 50);
         knop.setSize(50, 20);
         if (clicked == true) {
             r = Math.random();
@@ -63,17 +42,15 @@ public class H14_1 extends Applet {
                 default:
                     tekst = "" + shuffle;
 
-                    add(knop);
-                    knop.addActionListener(new KnopListener());
             }
-
-            }
-            repaint();
 
         }
+
+
+    }
         class KnopListener implements ActionListener {
             public void actionPerformed(ActionEvent e) {
-
+                clicked = true;
                 repaint();
             }
         }
