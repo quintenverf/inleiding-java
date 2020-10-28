6c340780c19e03e392abd2ea5e401acc6cf5b41d
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-10-28 13:38:54 +0100

    14_2 committed

--- src/H14/H14_2.java
+++ src/H14/H14_2.java
@@ -4,53 +4,53 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class H14_2 extends Applet {
 
-    String[] deck;
+    String kaart[] = {"Schoppen", "Harten", "Klaver", "ruiten"},
+           number[] = {"aas", "heer", "vrouw", "boer", "tien", "negen", "acht", "zeven", "zes", "vijf", "vrier", "drie", "twee"};
     Button knop = new Button("deel kaart");
     boolean clicked;
+    int getal, y, x, dertien;
+
+    ArrayList<String> card = new ArrayList<>();
 
     public void init() {
         setSize(1000,1000);
         clicked = false;
         add(knop);
         knop.addActionListener(new knopListener());
+        getal = 0;
 
     }
     public void paint(Graphics g) {
-        if (clicked == true){
-            deelKaart();
+
+        g.drawString("player 1", 50, 50);
+        g.drawString("player 2", 170,50);
+        g.drawString("player 3", 290, 50);
+        g.drawString("player 4", 410,50);
+        for (int teller = 0; teller < 52; teller++) {
+            if (teller == dertien) {x+=120;y=80;dertien +=13;}
+            g.drawString(card.get(teller),x,y);
+            y+= 20;
         }
+
     }
 
     class knopListener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
-            if (clicked == true);
+           x = 40;
+           y = 80;
+           dertien = 13;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 13; j++) {
+                    card.add(kaart[i] + " " + number[j]);
+                }
+            }
+            Collections.shuffle(card);
             repaint();
         }
-    }
-
-    private String deelKaart() {
-        int random = new Random().nextInt(deck.length);
-        String kaart = deck[random];
-
-        //vervang de inhoud van deck met een nieuwe lijst
-        //het nieuwe deck is 1 kaart kleiner dan het oude deck
-        String[] hulpLijst = new String[deck.length - 1];
-        //om het nieuwe deck te vullen hebben we een index nodig
-        int hulpindex = 52;
-        //kopieer alle kaarten naar hulplijst behalve de kaart die net gedeeld is
-        for (int teller = 0; teller < deck.length; teller++) {
-            if (teller != random) {
-                hulpLijst[hulpindex] = deck[teller];
-                hulpindex++;
-            }
-        }
-        //vervang nu deck met de nieuwe lijst
-        deck = hulpLijst;
-        //de methode geeft de random kaart terug
-        return kaart;
     }
 }
