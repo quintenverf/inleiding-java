742ccbda5397a73549bbfe4f25a436004570b52c
quintenverf <70647796+quintenverf@users.noreply.github.com>
2020-11-05 11:30:26 +0100

    14_praktijk committed

--- src/H14/H14_praktijk.java
+++ src/H14/H14_praktijk.java
@@ -10,74 +10,71 @@ public class H14_praktijk extends Applet {
     private Button play;
     private String game;
 
-    private int rest, comp, i, playing, random;
-    private int next[] = {21, 17, 13, 9, 5, 1};
-    private int x, y;
+    private int rest, comp, random, i, playing;
+    private int next[] = {21,17,13,9,5,1};
+    private int X, Y;
 
     public void init() {
-        Label label = new Label("how many smiley's do you choose(one,two or three)? ");
+        Label label = new Label("Hoeveel smileys neem je(één, twee of drie)?");
         input = new TextField("", 13);
-        play = new Button("play");
-        play.addActionListener(new playListener());
-
+        play = new Button("Speel");
+        play.addActionListener( new PlayListener() );
 
         add(label);
         add(input);
         add(play);
 
-        setSize(1000, 1000);
+
+
+        setSize(600,500);
         rest = 23;
         comp = 0;
         playing = 1;
 
-        x = 20;
-        y = 30;
-
+        X = 20;
+        Y = 30;
     }
-
     public void paint(Graphics g) {
         if (playing == 1) {
-            g.drawString("the computer has " + comp + " removed the smiley's", 10, 70);
-            g.drawString("amount of smiley left " + rest + " your turn", 10, 90);
+            g.drawString("De computer heeft " + comp + " smileys weggehaald.", 10, 70);
+            g.drawString("Aantal resterende smileys: " + rest + ". Jouw beurt.", 10, 90);
 
             for (int p = 0; p < rest; p++) {
-                if (p == 0 || p == 5 || p == 10 || p == 15 || p == 20) ;
-                y += 30;
-                x = 20;
+                if (p == 0 || p == 5 || p == 10 || p == 15 || p == 20) {
+                    Y += 30;
+                    X = 20;
+                }
+                X += 30;
             }
-            x += 30;
         } else if (playing == 2) {
             g.drawString(game, 10, 70);
             try {
                 Thread.sleep(4000);
-            } catch (Exception e) {
-            }
+            } catch (Exception e) {}
             play.setEnabled(true);
             playing = 1;
             repaint();
         } else if (playing == 3) {
             g.drawString(game, 10, 70);
+            try {
+                Thread.sleep(4000);
+            } catch (Exception e) {}
+            playing = 1;
+            repaint();
         }
-        try {
-            Thread.sleep(4000);
-        } catch (Exception e) {
-        }
-        playing = 1;
-        repaint();
     }
-
-    class playListener implements ActionListener {
+    class PlayListener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             int input = Integer.parseInt(H14_praktijk.this.input.getText());
-            x = 20;
-            y = 100;
+            X = 20;
+            Y = 100;
 
             if (input < 1 || input > 3) {
-                game = "invalid input";
+                game = "Invalid input!";
                 playing = 3;
             } else {
                 rest -= input;
-                if (rest == next[i]) {
+                if (rest < next[i]) {
                     i++;
                 }
                 int temp = rest - next[i];
@@ -90,7 +87,7 @@ public class H14_praktijk extends Applet {
                     if (temp > 3) {
                         double r = Math.random();
                         random = (int) (r * 3 + 1);
-                        random -= random;
+                        rest -= random;
                         comp = random;
                     } else {
                         rest -= temp;
@@ -104,7 +101,7 @@ public class H14_praktijk extends Applet {
                         rest -= random;
                         comp = random;
                     } else {
-                        rest += temp;
+                        rest -= temp;
                         comp = temp;
                     }
                     i++;
@@ -129,18 +126,17 @@ public class H14_praktijk extends Applet {
                 i = 0;
                 rest = 23;
                 playing = 2;
-                game = "you lost, better luck next time!";
+                game = "You lost!";
             }
             if (rest <= 0) {
 
                 play.setEnabled(false);
-                i = 10;
+                i = 0;
                 rest = 23;
                 playing = 2;
-                game = "you lost.... probably again.. ;)";
+                game = "You won!";
             }
             repaint();
         }
     }
 }
-
